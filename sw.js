36b0a0ff006bb9f77435e8b544d4d2d35ad9318a
GizRed <xgizredx@gmail.com>
2025-11-22 11:36:49 -0500

    Everything works

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for PWA and Push Notifications
-const CACHE_NAME = 'dmv-monitor-v19';
+const CACHE_NAME = 'dmv-monitor-v21';
 const urlsToCache = [
   '/app.js',
   '/manifest.json'
